e211901d83654fdd6b58fd58654ef1c195bf1d0e
Grigory Petrov <grigory.v.p@gmail.com>
2017-03-07 12:30:13 +0300

    Shorter code, video rendering example

--- Assets/Conference.cs
+++ Assets/Conference.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 using Voximplant;
 
 public class Conference : MonoBehaviour
@@ -14,28 +13,22 @@ public class Conference : MonoBehaviour
             if (granted) vox.connect(); // check audio and video permissions
         });
         vox.LogMethod += Debug.Log;
-        vox.ConnectionSuccessful += onConnectionSuccessfull;
-        vox.ConnectionFailedWithError += onConnectionFailed;
-        vox.LoginSuccessful += onLoginSuccessful;
-        vox.LoginFailed += onLoginFailed;
-        vox.CallConnected += OnCallConnected;
-        vox.CallDisconnected += OnCallDisconnected;
-        vox.CallFailed += OnCallFailed;
+        vox.ConnectionSuccessful += () => {
+            vox.login(new LoginClassParam("unity-demo-user@unity-demo-app." + ACC + ".voximplant.com", "unitydemopass"));
+        };
+        vox.LoginSuccessful += (name) => {
+            vox.call(new CallClassParam("*", false, false, ""));
+        };
+        vox.CallConnected += (callid, headers) => {
+            vox.beginUpdatingTextureWithVideoStream(VoximplantSDK.VideoStream.Remote, texture => {
+                // Assign texture to same object each frame, ex
+                // something.GetComponent<MeshRenderer>().material.mainTexture = texture;
+            });
+        };
+        vox.ConnectionFailedWithError += (reason) => { };
+        vox.LoginFailed += (reason) => { };
+        vox.CallConnected += (callid, headers) => { };
+        vox.CallDisconnected += (callid, headers) => { };
+        vox.CallFailed += (callid, code, reason, heasers) => { };
     }
-
-    private void onConnectionSuccessfull()
-    {
-        vox.login(new LoginClassParam("unity-demo-user@unity-demo-app." + ACC + ".voximplant.com", "unitydemopass"));
-    }
-
-    private void onLoginSuccessful(string name)
-    {
-        vox.call(new CallClassParam("*", false, false, ""));
-    }
-
-    private void onConnectionFailed(string reason) { }
-    private void onLoginFailed(LoginFailureReason reason) { }
-    private void OnCallConnected(string callid, Dictionary<string, string> headers) { }
-    private void OnCallDisconnected(string callid, Dictionary<string, string> headers) { }
-    private void OnCallFailed(string callId, int code, string reason, Dictionary<string, string> headers) { }
 }
